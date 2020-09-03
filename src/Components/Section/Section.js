7fd408854dde553d73969caee251695e4e8c5932
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-09-03 22:34:52 +0530

    code refactor

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -7,19 +7,17 @@ class Header extends React.Component {
     render(){
 
        return ( 
-        <div className="container">
-            <div className="container" id="about">
-            <section className={styles.about} data-aos="fade-up">
-                <h2>ABOUT ME</h2>
-                <div>
+            <div  className="container" id="about">
+            <section className={styles.about}>
+                <h2 data-aos="fade-up">ABOUT ME</h2>
+                <div data-aos="fade-up">
                     <img src={profile} alt='profile'/>
                 </div>
-                <p className="container text-center">
+                <p data-aos="fade-up" className="container text-center">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis temporibus labore tempora culpa molestiae illum eveniet consequatur aspernatur mollitia odio excepturi, eum error animi et sit consectetur aliquam doloremque, delectus aperiam 
                 </p>
             </section>
             </div>
-        </div>
        )
     }
 }
