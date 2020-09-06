f75b12cb172ee7e2114d2cbe3af5d529afa2aa64
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-09-06 21:59:20 +0530

    Added Source links

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+ 
+import './Cards.css'
 
 class Cards extends React.Component {
+
     render(){
 
        return ( 
@@ -11,7 +13,8 @@ class Cards extends React.Component {
                     <div className="card-body">
                         <h5 className="card-title">{this.props.title}</h5>
                             <p className="card-text">{this.props.text}</p>
-                        <a href={this.props.go} className="btn btn-success" target="_blank" rel="noopener noreferrer" >Visit</a>
+                        <a href={this.props.go} className="btn btn-outline-success" target="_blank" rel="noopener noreferrer" >Visit</a>
+                        <a href={this.props.source} className ="btn btn-outline-primary source" target="_blank" rel="noopener noreferrer">Source</a>
                     </div>
            </div>    
             </React.Fragment>
