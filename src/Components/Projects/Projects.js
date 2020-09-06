61a7975a0c82a5b918ec01a1b6371b17f4003e50
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-09-06 22:02:05 +0530

    added source prop

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -20,12 +20,12 @@ class Projects extends React.Component {
                </div>
                    
                <div className={styles.cards}>  
-                    <Cards src={Board} title={'Blog'} text={'General Discussion website'} go={'http://mohitboard.herokuapp.com/'}/>
-                    <Cards src={Weather} title={'Live Weather'} text={'Live weather details'} go={'https://mohitb07.github.io/live_weather/'}/>
-                    <Cards src={Corona} title={'Corona-Tracker'} text={'Daily Corona Tracker'} go={'https://mohitb07.github.io/corona-tracker/'}/>
-                    <Cards src={Google} title={'Google-Clone'} text={'Fully functional google clone'} go={'https://mohitb07.github.io/google/'}/>
-                    <Cards src={ConvertorV2} title={'Exchange Ratev2.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rate-V2.0/'}/>
-                    <Cards src={ConvertorV1} title={'Exchange Ratev1.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rateV1.0/'}/>
+                    <Cards src={Board} title={'Blog'} text={'General Discussion website'} go={'http://mohitboard.herokuapp.com/'}  source={'https://github.com/Mohitb07/mohitboard'}/>
+                    <Cards src={Weather} title={'Live Weather'} text={'Live weather details'} go={'https://mohitb07.github.io/live_weather/'} source={'https://github.com/Mohitb07/live_weather'}/>
+                    <Cards src={Corona} title={'Corona-Tracker'} text={'Daily Corona Tracker'} go={'https://mohitb07.github.io/corona-tracker/'} source={'https://github.com/Mohitb07/corona-tracker'}/>
+                    <Cards src={Google} title={'Google-Clone'} text={'Fully functional google clone'} go={'https://mohitb07.github.io/google/'} source={'https://github.com/Mohitb07/google'}/>
+                    <Cards src={ConvertorV2} title={'Exchange Ratev2.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rate-V2.0/'} source={'https://github.com/Mohitb07/exchange_rate-V2.0'}/>
+                    <Cards src={ConvertorV1} title={'Exchange Ratev1.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rateV1.0/'} source={'https://github.com/Mohitb07/exchange_rateV1.0'}/>
                </div>
                </div>
               
