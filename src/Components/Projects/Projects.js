bfbbe3922cb5f2494e46b6205369a2f0f334fa3e
mohitb07 <bmohit980@gmail.com>
2021-05-26 15:53:07 +0530

    new buttons

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,8 +6,9 @@ import Board from '../../Images/board.png'
 import Weather from '../../Images/live_weather.png'
 import Corona from '../../Images/corona-tracker.png'
 import Google from '../../Images/google.png'
-import ConvertorV2 from '../../Images/convertorv2.0.png'
-import ConvertorV1 from '../../Images/convertorv1.0.png'
+import bits from '../../Images/dirtybits.png'
+import api from '../../Images/api.png'
+
 
 class Projects extends React.Component {
     render(){
@@ -17,16 +18,19 @@ class Projects extends React.Component {
            <div className="container" data-aos="fade-up" >
                <div className={styles.newContainer}>
                <div className={styles.heading}>
-                <h2 className="text-center">Some Projects</h2>
+                <h2 style={{fontSize:'40px', paddingTop:'40px'}} className="text-center">Some Projects</h2>
                </div>
                    
                <div className={styles.cards}>  
-                    <Cards src={Board} title={'Blog'} text={'General Discussion website'} go={'http://mohitboard.herokuapp.com/'}  source={'https://github.com/Mohitb07/mohitboard'}/>
+                    <Cards src={bits} title={'DirtyBits'} text={'Online Coding Platform'} go={'https://dirtybits.herokuapp.com/'} source={'https://github.com/Mohitb07/Dirtybits'}/>
+                    <Cards src={Board} title={'Discussion Thread'} text={'General Discussion website'} go={'http://mohitboard.herokuapp.com/'}  source={'https://github.com/Mohitb07/mohitboard'}/>
+                    <Cards src={api} title={'Task Manager API'} text={'Authenticate User and CRUD their Posts'} go={'https://bisht-task-manager-api.herokuapp.com/'} source={'https://github.com/Mohitb07/Task-Manager-REST-API'}/>
                     <Cards src={Weather} title={'Live Weather'} text={'Live weather details'} go={'https://mohitb07.github.io/live_weather/'} source={'https://github.com/Mohitb07/live_weather'}/>
                     <Cards src={Corona} title={'Corona-Tracker'} text={'Daily Corona Tracker'} go={'https://mohitb07.github.io/corona-tracker/'} source={'https://github.com/Mohitb07/corona-tracker'}/>
                     <Cards src={Google} title={'Google-Clone'} text={'Fully functional google clone'} go={'https://mohitb07.github.io/google/'} source={'https://github.com/Mohitb07/google'}/>
-                    <Cards src={ConvertorV2} title={'Exchange Ratev2.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rate-V2.0/'} source={'https://github.com/Mohitb07/exchange_rate-V2.0'}/>
-                    <Cards src={ConvertorV1} title={'Exchange Ratev1.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rateV1.0/'} source={'https://github.com/Mohitb07/exchange_rateV1.0'}/>
+
+                    {/* <Cards src={ConvertorV2} title={'Exchange Ratev2.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rate-V2.0/'} source={'https://github.com/Mohitb07/exchange_rate-V2.0'}/>
+                    <Cards src={ConvertorV1} title={'Exchange Ratev1.0'} text={'Latest Exchange Rate'} go={'https://mohitb07.github.io/exchange_rateV1.0/'} source={'https://github.com/Mohitb07/exchange_rateV1.0'}/> */}
                </div>
                </div>
               
